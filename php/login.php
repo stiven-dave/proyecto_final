4b918e9fb36c0c3f0b4374060d42c429324e5dbd
stiven-dave <stivenalbeto10@gmail.com>
2025-12-12 16:10:10 -0500

    Guardad

--- php/login.php
+++ php/login.php
@@ -8,7 +8,7 @@ error_reporting(0);
 // ===============================
 // HEADERS (CORS)
 // ===============================
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
--- php/registro.php
+++ php/registro.php
@@ -88,7 +88,7 @@
 // ============================
 // HEADERS Y CORS
 // ============================
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
